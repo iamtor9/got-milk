18f20e62c3e0c81ab61f07a1d9588ff1641723a5
TabooThao <daniel.ntaj.thao@gmail.com>
2019-08-26 15:17:24 -0500

    changing jquery to vanilla, and making it look nicer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,45 @@
-// JavaScript function that wraps controls
-$(document).ready(function() {
+// This Variable is for the player
+// Confused at what the class stick is referring to since it only appears here - How does it function? ~~ Daniel
+var stickFigure = $(".stick");
 
-  var stickFigure = $(".stick");
-
-
-
-// ====================================================================================
-// ====================================================================================
-  // Keyboard move controls
-  $(document).keyup(function(e) {
+// This function is seperated so that we can disable it at some other point in time
+// We have it first so we refer to it
+function charMovement(e) {
+  document.addEventListener("keyup", function(e) {
     switch (e.which) {
 
-    // Move Buttons (Keyboard Down)
-    case 83:
-      stickFigure.animate({ top: "+=106.2px" }, 1);
-      break;
+      // Move Buttons (Keyboard Down)
+      case 83:
+        stickFigure.animate({ top: "+=106.2px" }, 1);
+        break;
+  
+        // Move Buttons (Keyboard Right)
+      case 68:
+        stickFigure.animate({ left: "+=100.4px" }, 1);
+        break;
+  
+        // Move Buttons (Keyboard Up)
+      case 87:
+        stickFigure.animate({ top: "-=106.2px" }, 1);
+        break;
+  
+        // Move Buttons (Keyboard Left)
+      case 65:
+        stickFigure.animate({ left: "-=100.4px" }, 1);
+        break;
+  
+      default:
+        break;
+      }
+  })
+}
 
-      // Move Buttons (Keyboard Right)
-    case 68:
-      stickFigure.animate({ left: "+=100.4px" }, 1);
-      break;
 
-      // Move Buttons (Keyboard Up)
-    case 87:
-      stickFigure.animate({ top: "-=106.2px" }, 1);
-      break;
-
-      // Move Buttons (Keyboard Left)
-    case 65:
-      stickFigure.animate({ left: "-=100.4px" }, 1);
-      break;
-
-    default:
-      break;
-    }
-  });
-});
-
+// JavaScript Vanilla function that wraps controls
+window.addEventListener("load", (function() {
+  charMovement();
+  console.log("Onload, activate charmovement JS Vanilla");
+}));
 
 // ====================================================================================
 // ====================================================================================
@@ -151,19 +154,35 @@ for(let i = 0; i < 153; i++) {
   tile.append(brock);
   };
 //Generates taxi's for the third world
-if(i === 19 || i === 20 || i === 21 || i === 22 || i === 23 || i === 24 || i === 25 || i === 26 || i === 27 || i === 28 || i === 29 || i === 30 || i === 31 || i === 32 || i === 33 || i === 103 || i === 104 || i === 105 || i === 106 || i === 107 || i === 108 || i === 109 || i === 110 || i === 111 || i === 112 || i === 113 || i === 114 || i === 115 || i === 116 || i === 117 || i === 118) {
+if(i === 19 || i === 20 || i === 21 || i === 22 || i === 23 || i === 24 || i === 25 || 
+   i === 26 || i === 27 || i === 28 || i === 29 || i === 30 || i === 31 || i === 32 || 
+   i === 33 || i === 103 || i === 104 || i === 105 || i === 106 || i === 107 || i === 108 || 
+   i === 109 || i === 110 || i === 111 || i === 112 || i === 113 || i === 114 || i === 115 || 
+   i === 116 || i === 117 || i === 118) {
   tile.append(taxi);
   };
 //Generates skulls for the fourth world
-if(i === 102 || i === 103 || i === 104 || i === 105 || i === 106 || i === 107 || i === 108 || i === 109 || i === 126 || i === 92 || i === 75 || i === 58 || i === 59 || i === 60 || i === 61 || i === 62 || i === 63) {
+if(i === 102 || i === 103 || i === 104 || i === 105 || i === 106 || i === 107 || i === 108 || 
+   i === 109 || i === 126 || i === 92 || i === 75 || i === 58 || i === 59 || i === 60 || i === 61 || 
+   i === 62 || i === 63) {
   tile.append(skull);
   };
 //Generates fish for the sixth
-if(i === 69 || i === 70 || i === 71 || i === 72 || i === 73 || i === 74 || i === 75 || i === 76 || i === 77 || i === 78 || i === 79 || i === 80 || i === 81 || i === 82 || i === 83 || i === 84 || i === 18 || i === 19 || i === 20 || i === 36 || i === 55 || i === 56 || i === 57 || i === 39 ||i === 24 || i === 25 || i === 42 || i === 26 || i === 61 || i === 62 || i === 63 || i === 45 ||i === 30 || i === 31 || i === 48 || i === 32 || i === 67 || i === 120 || i === 121 || i === 122 || i === 104 || i === 126 || i === 127 || i === 128 || i === 110 || i === 132 || i === 133 || i === 134 || i === 116 || i === 89 || i === 90 || i === 91 || i === 107 || i === 95 || i === 96 || i === 97 || i === 113 || i === 101 ){
+if(i === 69 || i === 70 || i === 71 || i === 72 || i === 73 || i === 74 || i === 75 || i === 76 || 
+   i === 77 || i === 78 || i === 79 || i === 80 || i === 81 || i === 82 || i === 83 || i === 84 || 
+   i === 18 || i === 19 || i === 20 || i === 36 || i === 55 || i === 56 || i === 57 || i === 39 ||
+   i === 24 || i === 25 || i === 42 || i === 26 || i === 61 || i === 62 || i === 63 || i === 45 ||
+   i === 30 || i === 31 || i === 48 || i === 32 || i === 67 || i === 120 || i === 121 || i === 122 || 
+   i === 104 || i === 126 || i === 127 || i === 128 || i === 110 || i === 132 || i === 133 || i === 134 || 
+   i === 116 || i === 89 || i === 90 || i === 91 || i === 107 || i === 95 || i === 96 || i === 97 || 
+   i === 113 || i === 101 ){
   tile.append(fish);
   };
   //Generates lava for the sixth world
-if(i === 69 || i === 70 || i === 71 || i === 72 || i === 73 || i === 74 || i === 75 || i === 76 || i === 77 || i === 78 || i === 79 || i === 80 || i === 81 || i === 82 || i === 83 || i === 84 || i === 20 || i === 19 || i === 56 || i === 57 || i === 26 || i === 25 || i === 62 || i === 63 ||i === 32 || i === 31 || i === 120 || i === 86 || i === 126 || i === 92 || i === 132 || i === 98) {
+if(i === 69 || i === 70 || i === 71 || i === 72 || i === 73 || i === 74 || i === 75 || i === 76 || i === 77 || 
+   i === 78 || i === 79 || i === 80 || i === 81 || i === 82 || i === 83 || i === 84 || i === 20 || i === 19 || 
+   i === 56 || i === 57 || i === 26 || i === 25 || i === 62 || i === 63 ||i === 32 || i === 31 || i === 120 || 
+   i === 86 || i === 126 || i === 92 || i === 132 || i === 98) {
   tile.append(lava);
   };
 
