da53947a09398851a8f907cfd517fce708180fca
Calah Hansen <calah.hansen@gmail.com>
2019-08-29 16:27:00 -0500

    modified images

---
+++ welcome_player.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome Player</title>
+</head>
+<body>
+    
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your favorite color is: <?php echo $_POST["favcolor"]; ?>
+
+</body>
+</html>
