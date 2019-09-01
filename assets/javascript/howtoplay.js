3e2625fd650917b79da5a1ddfcd2ab72e9b3c85d
Calah Hansen <calah.hansen@gmail.com>
2019-08-31 19:44:28 -0500

    working local forage and audio

--- assets/javascript/howtoplay.js
+++ assets/javascript/howtoplay.js
@@ -1,37 +1,31 @@
 //Global variables
-const age="8";
+var array = [];
+var age;
 
 //Create a function
 function submitInfo() {
+    document.getElementById("age").innerHTML = "";
     console.log("button was clicked");
     age = document.getElementById("age").value;
+    array.push(age);
 }
 document.querySelector("#submit").addEventListener("click", function () {
-    event.preventDefault();
-    submitInfo();
+    event.preventDefault(); //not sure why but if I remove this then the audio doesn't work
+    localforage.clear().then(submitInfo()); //cleared localforage here so it only saves age once
     //make an object to put the array
     let dataIn = {
         age: age,
     }
+    localforage.getItem("saved").then(function (result) {
+        if (!result) {
+        result = [];
+        }
+        result.push(dataIn);
+        localforage.setItem("saved", result)
+        .then(function () {
+            console.log(result);   
+    });
+    });
+
+
 });
-//     console.log(document.getElementById("submit"));
-//     console.log(dataIn);
-//     //const value = document.querySelector(".form-control").value;
-//     localforage.getItem("saved").then(function (result) {
-//       console.log(result);
-//       if (!result) {
-//         result = [];
-//       }
-//       result.push(dataIn);
-  
-//       localforage.setItem("saved", result)
-//         .then(function () {
-//           //console.log("saved");
-       
-//           console.log(result);
-//           displayTableRusults();   
-//         });
-//     });
-    
-   
-//   })
